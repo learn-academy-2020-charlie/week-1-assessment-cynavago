00b717c34163da680e4bf61c20d942cff8f90c11
cynavago <cnavarrohdz@gmail.com>
2020-07-02 16:52:48 -0700

    trying to push my files

--- code-challenges.js
+++ code-challenges.js
@@ -10,6 +10,32 @@ var temp1 = 35
 var temp2 = 350
 var temp3 = 212
 
+//create a function for boilingPoint of degrees
+const boilingPoint = (degrees) => {
+//create an if statement that determines if degrees are less than boiling point    
+    if (degrees < 212){
+//return using string interpolation
+        return `${degrees} is below boiling point`
+    }
+//create an else if statement that determines if degrees are above boiling point  
+    else if (degrees > 212){
+//return using string interpolation        
+        return `${degrees} is above boiling point`
+    }
+//create an if statement that determines if degrees are boiling point using
+    else if (degrees === 212){
+//return using string interpolation        
+        return `${degrees} is at boiling point`
+//return null if anything other than numbers are inputted 
+    } else {
+        return null
+    }
+}
+
+//run temperature variables
+console.log(boilingPoint(temp1));
+console.log(boilingPoint(temp2));
+console.log(boilingPoint(temp3));
 
 
 
@@ -18,15 +44,42 @@ var temp3 = 212
 
 var myNumbers1 = [3, 7, 0, 6, -9]
 
+// var arrayLength = array.length
 
+//create a function for the array
+const multBy5 = (array) =>{
+    //create a for loop for the array
+    // let
+    for(let i = 0; i<array.length; i++){
+     //return and multiply each value at each index by 5. not sure how to bring it back as an array.
+        // return (array[i]*5);
+        console.log(array[i]*5);
+    }
+    // return array.join("")
+};
 
+//log results
+console.log(multBy5(myNumbers1));
+
+//notes
+// var myArray = [1, 2, 3, 4, 5, 6]
+
+// for(let i = 0; i<myArray.length; i++){
+//   console.log(myArray[i]);
+// }
 
 // --------------------3) Create a function that multiplies each number in the array by 5 using map.
 // Use the test variable provided below. Expected outcome: [40, -35, 0, 30, 10]
 
 var myNumbers2 = [8, -7, 0, 6, 2]
 
+//created a function with an array
+const multipleBy5 = (array) => {
+//used .map to multiple the array by 5 and return a new array
+    return array.map (value => value*5)
+}
 
+console.log(multipleBy5(myNumbers2));
 
 
 // --------------------4) Create a function that takes in an array and returns an array with only the values at odd indexes.
@@ -34,7 +87,17 @@ var myNumbers2 = [8, -7, 0, 6, 2]
 
 var coffeeRun = ["Vanilla latte", "Chai tea", "Black coffee", "Mocha", "Americano", "Double espresso"]
 
+//declare a function that takes in an array
+const faveDrinks = (array) => {
+//return the array with the higher order function .filter()
+    return array.filter ((value, index) =>{
+//return the values of the array that have an odd index        
+        return index % 2 !== 0
+    })
+}
 
+//log the function
+console.log(faveDrinks(coffeeRun));
 
 
 // --------------------5) Create a function that removes all the vowels from a string.
@@ -43,12 +106,57 @@ var coffeeRun = ["Vanilla latte", "Chai tea", "Black coffee", "Mocha", "American
 var stringWithVowels1 = "HeyThereLearnStudent"
 var stringWithVowels2 = "ILoveJavaScript"
 
+//declare a function with an array
+const newString = (array) => {
+    //we need to convert the string into an array using split() so we can apply .filter() later on
+    let convertToArray = array.split("")
+    //we use higher order function .filter() to get rid of vowels
+    let noVowels = convertToArray.filter(value =>{
+        return value !== "a" && value !== "e" && value !== "i" && value !== "o" && value !== "u"
+    }
+    )
+    //conver the array back into a string and return the string
+    return noVowels.join("")       
+}
+
+console.log(newString(stringWithVowels1));
+console.log(newString(stringWithVowels2));
 
 
 
 // --------------------6) Expand the logic from #4 to inform your user if the variable passed into the vowel removal function is not a string.
 // Copy and paste or rewrite the function. Use the test variables provided below.
 
+
+var stringWithVowels1 = "HeyThereLearnStudent"
+var stringWithVowels2 = "ILoveJavaScript"
 var stringWithVowels3 = true
 var stringWithVowels4 = 42
 var stringWithVowels5 = "CodingIsGreat"
+
+
+
+
+//declare a function with an array
+const newString = (array) => {
+    // use type of to see if the array that user inputted is not a string, if its not, let user know its "not a string"
+    if (typeof array !== "string"){
+        return `Looks like ${array} is not a string. Plz try again.`
+    // if it is a string, then continue onto the code below    
+    } else {
+    //we need to convert the string into an array using split() so we can apply .filter() later on
+    let convertToArray = array.split("")
+    //we use higher order function .filter() to get rid of vowels
+    let noVowels = convertToArray.filter(value =>{
+        return value !== "a" && value !== "e" && value !== "i" && value !== "o" && value !== "u" 
+    }
+    )
+    //convert the array back into a string and return the string
+    return noVowels.join("") 
+}};      
+
+console.log(newString(stringWithVowels1));
+console.log(newString(stringWithVowels2));
+console.log(newString(stringWithVowels3));
+console.log(newString(stringWithVowels4));
+console.log(newString(stringWithVowels5));
--- interview-questions.md
+++ interview-questions.md
@@ -4,23 +4,34 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What is a function? Why would you use one?
 
-  Your answer: filters are cool
+  Your answer:
+  Functions are reusable pieces of code. Can have lots of different code inside such as arguments. They are like a giant umbrella of information and you can name each function.
 
   Researched answer:
-
+  Functions are blocks of code designed to perform a particular task. It is excevited when "something" invokes it. It always returns an output, can be used many times, and should be as generic as possible.
 
 
 2. What is the difference between map and filter?
 
   Your answer:
+  The difference is that .map() looks at each element in an array while .filter() has a true/false code that your array has to go through.
+
+  .map() is an array method, returns to us a completely new array.
+
+  .filter() is also an array method, like map, but it has a built in if/else statement so it looks for true/false values. It returns back what we "filtered" through it. 
 
   Researched answer:
 
+  The .map() method creates a new array with the results of calling a function for every array element. It calls the provided function once for each element in an array, in order. It does not execute without values. 
+
+  The .filter() method creates an array filled with all array elements that pass a test (provided as a function). Also does not execute without values.
 
 
 3. What is the difference between map/filter and a for loop?
 
   Your answer:
+  A for loop iterates from a defined start and end point with a defined number of times. Map/filter only go through the array once.
+
 
   Researched answer:
 
@@ -29,30 +40,33 @@ Answer the following questions. First, without external resources. Challenge you
 4. What is the difference between console.log() and return?
 
   Your answer:
+  console.log is used for developers, we shouldn't count on it when defining a function. It's helpful when testing out a piece of code. A return would live inside a function and can be used in reference inside the function.
 
   Researched answer:
-
+  console.log prints out information to the console. returns a call to pass some value back up to where the call was made. 
 
 
 5. In regards to functions, what is an argument?
 
   Your answer:
+  Parameters. They are used in a function as placeholders for information.
 
   Researched answer:
-
+  The parameters, in a function call, are the function's arguments. JavaScript arguments are passed by value. The function only gets to know the values, not the argument's locations. If a function changes an argument's value, it does not change the parameter's original value.
 
 
 6. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
   Your answer:
+  Pair programming allows for partners to share and equally participate when coding. They take turns being the driver, where a partner is working on the live code. The driver then pushes the code up for the other partner to have access to it. It is about working together on a single project.
 
   Researched answer:
-
+  Pairing involves having 2 programmers working at a single workstation. One programmer "drive" aka operates the keyboard while the other "navigates", watches, learns, asks, talks and makes suggestions. In theory, the driver focuses on the code at hand and the navigator focuses on a level of abstraction thats higher. 
 
 
 7. What is something we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: I loved learning about higher order functions! They make life easier ^_^ It's cool to know that there are certain built-in methods that already do most of the leg work. I also really enjoyed the team building excercises. It was fun to see everyone laugh and be silly. 
 
 
 
